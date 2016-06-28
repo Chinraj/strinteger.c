89acbdddae1e01f7ad2e0a20115b83b20a28734c
Chinraj.K <chinrajkalimuthu@gmail.com>
2016-06-28 13:29:50 +0530

    Create strinteger.c

---
+++ strinteger.c
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>																																																													#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int b;
+char string[5];																																																																																					clrscr();
+printf("Enter string value :");
+scanf("%s",string);
+b=atoi(string);
+printf("Integer :%d \n",b);
+getch();
+}
